fe6a36782d9c3bded95ca77c66f9117228a1c26b
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2026-09-12 20:02:19 -0500

    constructor

--- contracts/5_VariableSimples.sol
+++ contracts/5_VariableSimples.sol
@@ -5,6 +5,9 @@ pragma solidity >=0.8.2 <0.9.0;
 contract VariableSimples{
     uint256  public edad; //por defecto toma el valor de cero
 
+    constructor(uint256 _edad) {
+        edad = _edad;
+    }
     function cambiarEdad(uint256 _edad) public {
         edad = _edad;
     }
